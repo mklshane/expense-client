137d354e08591271fc292f99af1db79cabc72180
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2026-04-22 23:25:28 +0800

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Apps from './Apps.jsx'
+import Apps from './Test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
